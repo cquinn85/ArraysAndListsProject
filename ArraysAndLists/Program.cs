a15dcca172df0b9e26baf49fc1ec477cddb323ad
cquinn85 <cequinn300@gmail.com>
2020-10-30 20:40:59 -0500

    arrays and lists complete

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -8,12 +9,17 @@ namespace ArraysAndLists
         {
             //TODO:
 
-            // Create an int Array and populate numbers 1-10
+            // DONE: Create an int Array and populate numbers 1-10
 
-            /* Create two Lists of type int.
+            var numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            /* DONE: Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+            var evens = new List<int>();
+            var odds = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +28,48 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            //for(int i = 0; i < numbers.Length; i++)
+           // {
+             //   if(numbers[i] % 2 == 0)
+           //     {
+            //        evens.Add(i);
+            //    }
+            //    else
+             //   {
+             //       odds.Add(i);
+             //   }
+            //}
+                       //OR
+
+            foreach(var num in numbers)
+            {
+                if(num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
+
+
+            
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+
+            foreach (var item in evens)
+            {
+                Console.WriteLine($"{item} is an even number");
+            }
+
+            foreach(var item in odds)
+            {
+                Console.WriteLine($"{item} is an odd number.");
+            }
         }
     }
 }
